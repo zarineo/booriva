470ff982621fb2d6d349df211d7439fbfa49833a
zarineo <tochilka007@mail.ru>
2024-03-21 14:27:19 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Абалдеть можно просто</p>
+        <h3>Абалдевать закончили, в какой папке теперь работать алиш</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
